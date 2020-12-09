d7ea5597ca1a508cd2c3a46b6c04f1e4977cfe7a
Mrgibsonict <62545043+Mrgibsonict@users.noreply.github.com>
2020-12-09 23:31:08 +0000

    Initial commit

---
+++ questions.json.js
@@ -0,0 +1,260 @@
+var jsonData = [
+  {
+    "Question": "Why do customers needs vary?",
+    "answer": "• Benefits they require  <br />• Amount of money they are able/willing to pay  <br />• Quantity of goods they require  <br />• Quality of goods they require  <br />• Time and location they wish to purchase the goods",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the 6 main types of market segmentation?",
+    "answer": "• Age  <br />• Gender  <br />• Occupation  <br />• Income  <br />• Geographic  <br />• Lifestyle",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the benefits to a business of market segmentation?",
+    "answer": "• Ensures customer needs are matched and met  <br />• Potential for increased profits/profitability  <br />• Increased customer retention  <br />• Allows for targeted marketing  <br />• Potential for an increase in market share",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the purposes to a business of condducting market research?",
+    "answer": "• To reduce risk  <br />• To understand the market  <br />• To promote the organisation  <br />• To aid decision making  <br />• To gain customers’ views and understand their needs<br />• To inform product development",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main Primary (Field) Market Research methods",
+    "answer": "• Observations  <br />• Questionnaires  <br />• Surveys  <br />• Focus groups  <br />• Consumer trials",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main Secondary (desk) Market Research Methods?",
+    "answer": "• Internal data  <br />• Books/newspapers/trade magazines  <br />• Competitors’ data  <br />• Government publications and statistics  <br />• Purchased research material (e.g. Mintel)",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "What types of customer feedback techniques are available to a business start-up",
+    "answer": "• Social media/online communities with reviews and  comments  <br />• Online surveys  <br />• Customer comment cards  <br />• Comments made to staff members  <br />• Telephone/email surveys  <br />• Email contact forms",
+    "Topic": "LO1 - Understand how to target a market",
+    "subtopic": ""
+  },
+  {
+    "Question": "Define Fixed Costs",
+    "answer": "Costs that do not vary with the level of output",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "Define Variable Costs",
+    "answer": "Costs that do vary with the level of output",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "Give some common examples of Fixed Costs",
+    "answer": "rent<br />loan repayment<br />insurance<br />advertising<br />salaries<br />utilities",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "Give some common examples of Variable Costs",
+    "answer": "raw materials<br />components<br />stock<br />packaging",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "How do you calculate Total Costs?",
+    "answer": "Total Costs=Fixed Costs + Variable Costs",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "How do you calculate Revenue from sales?",
+    "answer": "Revenue=Selling Price x Number of units sold",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "Define Break-even",
+    "answer": "The level of output where total Revenue = Totals costs",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is the break-even formula?",
+    "answer": "Fixed Costs/(Selling price-Variable Cost)",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "How is profit calculated?",
+    "answer": "Revenue - Total Costs",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "How is profit per unit calculated?",
+    "answer": "Selling price per unit-total costs per unit",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "Why do businesses need to know the break-even point?",
+    "answer": "They can work out how many units they need to sell to break even and then decide whether their business idea is viable, They can use the data to inform business decisions on selling price, The can use the data to help convince/persuade a potential investor/lender",
+    "Topic": "LO2 - Understand what makes a product or service viable",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the 5 stages of the Product Life Cycle?",
+    "answer": "• Development  <br />• Introduction  <br />• Growth  <br />• Maturity  <br />• Decline",
+    "Topic": "LO3 - Understanding product development",
+    "subtopic": ""
+  },
+  {
+    "Question": "Identify and explain 5 possible Extensions strategies for products in the  product life cycle",
+    "answer": "• Advertising  <br />• Price changes  <br />• Adding value (e.g. improving the specification of an existing  product)  <br />• Exploration of new markets (e.g. new geographic market,  new target markets)  <br />• New packaging",
+    "Topic": "LO3 - Understanding product development",
+    "subtopic": ""
+  },
+  {
+    "Question": "Identify how a business can differentiate its products",
+    "answer": "Establishing a strong brand image for goods or services<br />Design mix model  - i.e. the variables that contribute to successful product  design - function, cost and appearance<br />Identifying a clear unique selling point (USP)  <br />Offering improved:  <br />- Location  <br />- Features  <br />- Functions  <br />- Design  <br />- Appearance  <br />- Selling price",
+    "Topic": "LO3 - Understanding product development",
+    "subtopic": ""
+  },
+  {
+    "Question": "Explain the 3 main external factors that can impact on product development in a company",
+    "answer": "• Technological developments (e.g. developments in  technology that affect production capabilities and consumer  preferences)  <br />• Economic issues (i.e. recession, boom and their effects)  <br />• Legal issues (i.e. copyright and patent, product safety  standards)",
+    "Topic": "LO3 - Understanding product development",
+    "subtopic": ""
+  },
+  {
+    "Question": "What 3 factors must a company sonsider when pricing a product to attract and retain customers?",
+    "answer": "• Income levels of target customers  <br />• Price of competitor products  <br />• Cost of production",
+    "Topic": "LO4 - Understand how to attract and retain customers",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main types of pricing strategies that businesses tend to use when pricing their products?",
+    "answer": "• Competitive pricing  <br />• Psychological pricing  <br />• Price skimming  <br />• Price penetration",
+    "Topic": "LO4 - Understand how to attract and retain customers",
+    "subtopic": ""
+  },
+  {
+    "Question": "What types of advertising methods are used to attract and retain customers?",
+    "answer": "• Leaflets  <br />• Social Media  <br />• Websites  <br />• Newspapers  <br />• Magazines  <br />• Radio",
+    "Topic": "LO4 - Understand how to attract and retain customers",
+    "subtopic": ""
+  },
+  {
+    "Question": "What Sales Promotion techniques can a company use to attract and retain customers?",
+    "answer": "• Discounts  <br />• Competitions  <br />• Buy one get one free (BOGOF)  <br />• Point of sale advertising  <br />• Free gifts/product trials  <br />• Loyalty schemes",
+    "Topic": "LO4 - Understand how to attract and retain customers",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the 3 main methods of customer service that a business can use to attract and retain customers?",
+    "answer": "• Product knowledge  <br />• Customer engagement (e.g. presentation, communication  skills)  <br />• After sales service",
+    "Topic": "LO4 - Understand how to attract and retain customers",
+    "subtopic": ""
+  },
+  {
+    "Question": "Identify the main forms of ownership for start-up businesses",
+    "answer": "• Sole trader  <br />• Partnership, including limited liability partnerships  <br />• Franchise",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main legal requirements of Sole Traders and Partnerships when starting up",
+    "answer": "Must register themselves as self employed with HMRC",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What additional legal requirements are involved when setting up a LLP?",
+    "answer": "Must send a Memorandum and Articles of Association to Companies House and receive a Certificate of Incorporation back before they can start trading",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is meant by unlimited liability?",
+    "answer": "The owners are personally responbible for all of the company's debts and their personal assets could be at risk",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is meant by limited liability?",
+    "answer": "Personal liability of the owners is limited only to the amount of money they invested into the company",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the 6 main sources of finance for a start-up business?",
+    "answer": "• own savings  <br />• friends and family  <br />• loans  <br />• crowdfunding  <br />• small business grants  <br />• business angels",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "Why are business plans so important?",
+    "answer": "to clarify a business idea to others (e.g. to secure funding) <br />to measure progress towards goals (e.g. timescales, sales  forecasts)  <br /> to help manage cash flow  <br /> to help identify potential problems (e.g. financial shortages)",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What should a business plan contain?",
+    "answer": "business objectives  <br />business strategies  <br />sales plan  <br />marketing plan  <br />financial forecasts",
+    "Topic": "LO5 - Understand factors for consideration when starting up a business",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is the main purpose of Human Resources?",
+    "answer": "responsible for all aspects of managing individuals who  work within a business",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is the main purpose of Marketing?",
+    "answer": "responsible for identifying the needs and wants of business  customers and developing products/services to meet those  needs",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is the main purpose of Operations?",
+    "answer": "organising the process that turns inputs into  outputs/finished goods that can be sold to customers",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What is the main purpose of Finance?",
+    "answer": "managing the financial resources in a small business and  reporting on financial performance",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main activities of Human Resources?",
+    "answer": "Recruitment and selection of employees  <br />Training and development of employees  <br />Performance management of employees  <br />Responsibility for health and safety in the workplace  <br />Ensuring compliance with employment legislation",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main activities of Marketing?",
+    "answer": "Market research, i.e. to research the market and find out customer opinions  <br />Developing a marketing mix: Product, Price, Place, Promotion (4Ps)",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main activities of Operations?",
+    "answer": "Production planning<br />Producing the product or service<br />Quality control<br />Stock control<br />Logistics",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  },
+  {
+    "Question": "What are the main activities of Finance?",
+    "answer": "Organisation and allocation of financial resources<br />Financial performance reporting<br />Monitoring of cash flow",
+    "Topic": "LO6 - Understand different functional activities needed to support a business start-up",
+    "subtopic": ""
+  }
+]
